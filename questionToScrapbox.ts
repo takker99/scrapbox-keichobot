463963070a4470e10a7fbe715ddf0d7dba41087b
takker99 <37929109+takker99@users.noreply.github.com>
2024-01-10 12:32:44 +0900

    fix: 引用符が予め付与されているので不要

--- questionToScrapbox.ts
+++ questionToScrapbox.ts
@@ -7,9 +7,5 @@ export const questionToScrapbox = (question: string): string => {
   const splitted = question.split("\n\n");
   const q = splitted[1] ?? splitted[0];
   const quote = question === splitted[0] ? "" : splitted[0];
-  return quote
-    ? [...quote.trim().split("\n").map((line) => `> ${line}`), q].join(
-      "\n",
-    )
-    : q;
+  return quote ? [quote.trim(), q].join("\n") : q;
 };
